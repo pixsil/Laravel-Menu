32c7a561d1b3c27c49965e0f7506afda776a65dd
pixsil <57232302+pixsil@users.noreply.github.com>
2022-10-25 16:55:28 +0200

    Update MenuMiddleware.php

--- Http/Middleware/MenuMiddleware.php
+++ Http/Middleware/MenuMiddleware.php
@@ -17,7 +17,7 @@ class MenuMiddleware
      */
     public function handle($request, Closure $next, $role, $menu)
     {
-        app(MenuFactory::class)->setActive($menu);
+        \Menu::setActive($menu);
 
         return $next($request);
     }
